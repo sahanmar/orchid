c51c68b851f5a047e8cd54b359a525696d8712cc
vlbelov <vlbelov@deloittece.com>
2022-12-19 00:44:01 +0100

    moving criterion evaluation to be a separate function

--- manifold/linear.py
+++ manifold/linear.py
@@ -30,15 +30,26 @@ class BasePCA(ManifoldLearningModule):
         embeddings = self.pca_layer(inputs)
         return embeddings  # [n, target_dim]
 
+    def evaluate_criterion(
+        self,
+        inputs: torch.Tensor,
+        embeddings: torch.Tensor,
+    ) -> torch.Tensor:
+        inputs_reconstructed = self.loss.reconstruct_from_linear(
+            embeddings=embeddings,
+            linear_layer=self.pca_layer,
+        )
+        loss_ = self.loss(inputs=inputs, outputs=inputs_reconstructed)
+        return loss_
+
     def train_step(self, inputs: torch.Tensor) -> Dict[str, torch.Tensor]:
         self.optimizer.zero_grad()
         with torch.enable_grad():
             embeddings = self(inputs)
-            inputs_reconstructed = self.loss.reconstruct_from_linear(
+            loss_step = self.evaluate_criterion(
+                inputs=inputs,
                 embeddings=embeddings,
-                linear_layer=self.pca_layer,
             )
-            loss_step = self.loss(inputs=inputs, outputs=inputs_reconstructed)
             loss_step.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
