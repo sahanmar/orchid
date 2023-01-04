b5b94e324f29deb79172c21e564d346bbe13ac6a
Marko <marko.sahan@gmail.com>
2023-01-04 10:48:54 +0100

    comment fixes

--- coref/test_logging_utils.py
+++ coref/test_logging_utils.py
@@ -4,14 +4,16 @@ from config import Config
 
 def test_get_stream_logger(config: Config) -> None:
     text = "Test file is created"
+    experiment = "test_run"
     logger = get_stream_logger(
-        logging_conf=config.logging, experiment="test_run"
+        logging_conf=config.logging, experiment=experiment
     )
     logger.info(text)
     assert config.logging.log_folder.is_dir()
     for log_file in config.logging.log_folder.iterdir():
-        with open(log_file, "r") as f:
-            log_text = [l.strip() for l in f.readlines()]
-        assert text in log_text
+        if str(log_file).startswith(experiment):
+            with open(log_file, "r") as f:
+                log_text = [l.strip() for l in f.readlines()]
+            assert text in log_text
         log_file.unlink()
         assert not log_file.is_file()
