9ea2364b1a681dca8e7e6c0af141715c947d10d2
Marko <marko.sahan@gmail.com>
2023-01-07 12:55:18 +0100

    tiny fix

--- active_learning/acquisition_functions.py
+++ active_learning/acquisition_functions.py
@@ -8,5 +8,8 @@ def random_sampling(instances: List[Doc], batch_size: int) -> SampledData:
     """
     Provides random sampling from given instances
     """
-    indices = sample(list(range(0, len(instances))), batch_size)
+    if len(instances) < batch_size:
+        indices = list(range(0, len(instances)))
+    else:
+        indices = sample(list(range(0, len(instances))), batch_size)
     return SampledData(indices, [instances[i] for i in indices])
