5f72ffcc10f2a2ca78294c2d22b5bcaf3c57dd91
Marko <marko.sahan@gmail.com>
2023-02-03 15:28:26 +0100

    resolve mutatuibs in simulations

--- active_learning/active_learning_simulation.py
+++ active_learning/active_learning_simulation.py
@@ -2,14 +2,18 @@ from coref.models import GeneralCorefModel
 from coref.const import Doc, SampledData
 from coref.models import load_coref_model, GeneralCorefModel
 from config import Config
+from typing import Tuple
+from copy import deepcopy
 
 
 def get_training_iteration_docs(
     docs: list[Doc], sampled_docs: SampledData
-) -> list[Doc]:
+) -> Tuple[list[Doc], list[Doc]]:
     for i, doc in zip(sampled_docs.indices, sampled_docs.instances):
         docs[i].simulation_token_annotations = doc.simulation_token_annotations
-    return [doc for doc in docs if doc.simulation_token_annotations.tokens]
+    return [
+        doc for doc in docs if doc.simulation_token_annotations.tokens
+    ], docs
 
 
 def run_simulation(
@@ -23,7 +27,9 @@ def run_simulation(
     al_config = config.active_learning
     # Training split
     sampled_data = model.sample_unlabled_data(train_docs)
-    training_data = get_training_iteration_docs(train_docs, sampled_data)
+    training_data, train_docs = get_training_iteration_docs(
+        deepcopy(train_docs), sampled_data
+    )
 
     # First training
     model.train(docs=training_data, docs_dev=dev_docs)
@@ -35,7 +41,9 @@ def run_simulation(
         model._logger.info(f" AL SIMULATION | round: {i}\n")
         # Prepare the data
         sampled_data = model.sample_unlabled_data(train_docs)
-        training_data = get_training_iteration_docs(train_docs, sampled_data)
+        training_data, train_docs = get_training_iteration_docs(
+            deepcopy(train_docs), sampled_data
+        )
 
         # Train the model
         model = load_coref_model(config)
