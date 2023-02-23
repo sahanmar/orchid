30ad4184d4c8a4af4d1f2e86df1ae5b8c4419d86
vlbelov <vlbelov@deloittece.com>
2023-02-23 19:31:06 +0100

    improved path processing

--- config/config.py
+++ config/config.py
@@ -11,9 +11,12 @@ from transformers import AutoTokenizer, AutoModel
 
 from config.active_learning import ActiveLearning
 from config.config_utils import overwrite_config, get_overwrite_value
+from config.logging import Logging
 from config.metrics import Metrics
 from coref.bert import load_bert
-from config.logging import Logging
+
+# Path to the source folder (repository root) determined automatically
+__src_path__ = Path(__file__).resolve().parents[1]
 
 
 @dataclass
@@ -33,8 +36,20 @@ class Data:
     num_of_training_docs: int = field(init=False)
 
     def __post_init__(self) -> None:
+        # Process paths
+        self.train_data = self._make_data_path_absolute(path=self.train_data)
+        self.dev_data = self._make_data_path_absolute(path=self.dev_data)
+        self.test_data = self._make_data_path_absolute(path=self.test_data)
+
+        # Count the amount of training documents
         with open(self.train_data, "r") as f:
             self.num_of_training_docs = sum(1 for _ in f)
+
+    @staticmethod
+    def _make_data_path_absolute(path: Path) -> Path:
+        if not path.is_absolute():
+            path = __src_path__.joinpath(path)
+        return path
 
     @staticmethod
     def load_config(
@@ -99,9 +114,15 @@ class ManifoldLearningParamsStandalone:
 @dataclass
 class ManifoldLearningParams:
     enable: bool
+    # Loss function to use
     loss_name: str
+    # Loss multiplier to scale it accordingly
     loss_alpha: float
+    # Whether to compute loss in the forward step
+    loss_in_forward: bool
+    # The fraction by which the output dimensionality should be reduced
     reduction_ratio: float
+    # Setup for the standalone usage of the manifold learning sub-package
     standalone: ManifoldLearningParamsStandalone
     verbose_outputs: List[str] = field(default_factory=list)
 
@@ -213,6 +234,10 @@ class Config:  # pylint: disable=too-many-instance-attributes, too-few-public-me
 
     @staticmethod
     def load_default_config(section: Optional[str]) -> "Config":
+        config_path = str(__src_path__.joinpath("config.toml"))
         if section is not None:
-            return Config.load_config("config.toml", section)
-        return Config.load_config("config.toml")
+            return Config.load_config(
+                config_path=config_path,
+                section=section,
+            )
+        return Config.load_config(config_path=config_path)
