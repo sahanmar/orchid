1022bdb15598f751611550b2a994cbda6366dcc8
vlbelov <vlbelov@deloittece.com>
2023-02-23 19:31:20 +0100

    computation of loss in the forward step

--- manifold/linear.py
+++ manifold/linear.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 from tqdm.auto import tqdm
 
 from config.config import ManifoldLearningParams
-from .base import ManifoldLearningModule
+from .base import ManifoldLearningModule, ManifoldLearningForwardOutput
 from .dataset import ManifoldDataloader
 
 
@@ -26,9 +26,18 @@ class BasePCA(ManifoldLearningModule):
             params=self.parameters(), lr=self.args.standalone.learning_rate
         )
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        embeddings = self.pca_layer(inputs)
-        return embeddings  # [n, target_dim]
+    def forward(self, inputs: torch.Tensor) -> ManifoldLearningForwardOutput:
+        embeddings = self.pca_layer(inputs)  # [n, target_dim]
+        if self.args.loss_in_forward:
+            loss = self.evaluate_criterion(
+                inputs=inputs,
+                embeddings=embeddings,
+            )
+        else:
+            loss = None
+
+        output = ManifoldLearningForwardOutput(embeddings=embeddings, loss=loss)
+        return output
 
     def evaluate_criterion(
         self,
@@ -38,23 +47,28 @@ class BasePCA(ManifoldLearningModule):
         inputs_reconstructed = self.loss.reconstruct_from_linear(
             embeddings=embeddings,
             linear_layer=self.pca_layer,
-        )
+        )  # [n, input_dim]
         loss_ = self.loss(inputs=inputs, outputs=inputs_reconstructed)
+
+        # Multiply the output loss with a scaling factor
         loss_ *= self.loss_alpha
         return loss_
 
-    def train_step(self, inputs: torch.Tensor) -> Dict[str, torch.Tensor]:
+    def train_step(self, inputs: torch.Tensor) -> ManifoldLearningForwardOutput:
         self.optimizer.zero_grad()
+        assert self.args.loss_in_forward, (
+            f"The loss has to be computed in the forward step "
+            f"to preserve memory"
+        )
         with torch.enable_grad():
-            embeddings = self(inputs)
-            loss_step = self.evaluate_criterion(
-                inputs=inputs,
-                embeddings=embeddings,
-            )
-            loss_step.backward()
+            result: ManifoldLearningForwardOutput = self(inputs)
+            assert (
+                result.loss is not None
+            ), f"Step loss is missing in the results"
+            result.loss.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
-        return {"embeddings": embeddings, "loss": loss_step}
+        return result
 
     def train_on_inputs(self, inputs: torch.Tensor) -> None:
         pbar = tqdm(range(self.args.standalone.epochs))
@@ -69,7 +83,7 @@ class BasePCA(ManifoldLearningModule):
                 res = self.train_step(inputs=b)
                 pf = {
                     k_: v_.detach().numpy()
-                    for k_, v_ in res.items()
+                    for k_, v_ in res.as_dict().items()
                     if k_ in self.args.verbose_outputs
                 }
                 pbar.set_postfix(pf)
